98b37d6b86e3711e5c72f44211b03a0134a1badc
Simona Tsvetkova <simonatsvetkova@gmail.com>
2019-05-19 21:55:20 +0300

    model and view creation

---
+++ carpooling/accounts/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    # username = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True, max_length=30)
+    last_name = forms.CharField(required=True, max_length=30)
+
+    # password1 = forms.CharField(required=True)
+    # password2 = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+
+    def save(self, commit=True):
+        if not commit:
+            raise NotImplementedError("Can't create User and UserProfile without database save")
+        user = super(RegistrationForm, self).save(commit=True)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.is_active = True
+        user.save()
+
+        # if commit:
+        #     user.save()
+        return user
+
+    # def save(self, commit=True):
+    #     user = User.objects.create_user(
+    #         self.cleaned_data['username'],
+    #         self.cleaned_data['email'],
+    #         self.cleaned_data["first_name"],
+    #         self.cleaned_data["last_name"],
+    #         self.cleaned_data['password1']
+    #     )
+    #     user.is_active = True
+    #     return user
