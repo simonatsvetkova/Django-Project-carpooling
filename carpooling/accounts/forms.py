0f0ffd036e9fa1e4ec5b6d01deae7cdcc259f5b6
Simona Tsvetkova <simonatsvetkova@gmail.com>
2019-05-20 16:56:55 +0300

    enhancements to ProfileUser model and Registration form

--- carpooling/accounts/forms.py
+++ carpooling/accounts/forms.py
@@ -7,17 +7,14 @@ from .models import ProfileUser
 
 
 class RegistrationForm(UserCreationForm):
-    # username = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True, max_length=30)
     last_name = forms.CharField(required=True, max_length=30)
 
-    # password1 = forms.CharField(required=True)
-    # password2 = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+        fields = ("first_name", "last_name",  "email", "username","password1", "password2")
 
         # def clean_username(self):
         #     username = self.cleaned_data["username"]
@@ -26,6 +23,15 @@ class RegistrationForm(UserCreationForm):
         #     except ProfileUser.DoesNotExist:
         #         return username
         #     raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    # checks if email is already used and returns message if it's not unique
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        print(User.objects.filter(email=email).count())
+        if email and User.objects.filter(email=email).count() > 0:
+            raise forms.ValidationError(f'This email address is already registered.')
+        return email
 
     def save(self, commit=True):
         if not commit:
@@ -38,17 +44,4 @@ class RegistrationForm(UserCreationForm):
         user.is_staff = True
         user.save()
 
-        # if commit:
-        #     user.save()
         return user
-
-    # def save(self, commit=True):
-    #     user = User.objects.create_user(
-    #         self.cleaned_data['username'],
-    #         self.cleaned_data['email'],
-    #         self.cleaned_data["first_name"],
-    #         self.cleaned_data["last_name"],
-    #         self.cleaned_data['password1']
-    #     )
-    #     user.is_active = True
-    #     return user
--- carpooling/accounts/models.py
+++ carpooling/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -8,10 +10,20 @@ class ProfileUser(models.Model):
     # REQUIRED_FIELDS = ('user',)
     # USERNAME_FIELD = 'username'
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     # username = User.username
     # profile_picture = models.URLField(default='https://upload.wikimedia.org/wikipedia/common/7/72/Default-welcomer.png')
     profile_picture = models.ImageField(upload_to='images/', default='https://upload.wikimedia.org/wikipedia/common/7/72/Default-welcomer.png')
 
     def __str__(self):
         return f"{self.user}"
+
+# this saves the user in both groups User (the default Django group) and the ProfileUser group
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, *args, **kwargs):
+    if not created:
+        return
+    ProfileUser.objects.create(user=instance)
+
+
+post_save.connect(create_or_update_user_profile, sender=User)
